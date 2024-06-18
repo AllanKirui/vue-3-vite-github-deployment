fdd6883cb92be83ac066e5bf26ea9d03603a5261
Allan Kirui <allan.kirui@outlook.com>
2024-06-19 02:31:09 +0300

    Add guide description

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    <h1>This is an about page</h1>
+    <h1 class="title">From Frustration to Success: Vue 3 Vite GitHub Deployment</h1>
+
+    <div class="about">
+      <p>
+        After struggling to deploy my Vue 3 Vite project to GitHub Pages, I created a small app to
+        figure it out. Here's a streamlined guide for setting up a Vue 3 Vite app with dynamic
+        routing and deploying it to GitHub.
+      </p>
+      <p>
+        If you are feeling frustrated like I was, this guide is here to help. I hope it makes the
+        process easier for you.
+      </p>
+      <p>- <a href="https://allankirui.github.io" target="_blank">Allan Kirui</a></p>
+    </div>
+
+    <p class="foot-note">Coding is Your Superpower ✌</p>
   </div>
 </template>
